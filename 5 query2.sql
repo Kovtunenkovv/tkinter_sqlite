89fd6889e3d3d4d4c9ba71856827426b5f115b8a
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2023-09-25 08:59:02 +0300

    Create 5 query2.sql

---
+++ 5 query2.sql
@@ -0,0 +1,14 @@
+CREATE TEMPORARY TABLE p as 
+select zip, hostname, eventid,firstoccurrence,enddate, substr(firstoccurrence,-9,-11) as date from alarm_daily 
+where ZIP = 'SEA' and (eventid = 'POWER') and firstoccurrence like '2023%'
+group by firstoccurrence, hostname
+;
+
+CREATE TEMPORARY TABLE h as select zip, hostname, eventid,firstoccurrence,enddate,substr(additionalinfo1,-12,12) as du from alarm_daily 
+where ZIP = 'SEA' and (eventid = 'Heartbeat Failure') and firstoccurrence like '2023%'
+group by firstoccurrence, hostname
+;
+
+select p.zip, p.hostname, p.eventid ,p.firstoccurrence,p.enddate,((Cast((JulianDay((p.enddate)) - JulianDay(p.firstoccurrence)) * 24 * 60 * 60 As Integer))) as delta, h.eventid, h.firstoccurrence,h.enddate,((Cast((JulianDay((h.enddate)) - JulianDay(h.firstoccurrence)) * 24 * 60 * 60 As Integer))) as delta1,du from p
+left join h on p.hostname = h.hostname and h.firstoccurrence BETWEEN p.firstoccurrence and p.enddate
+where h.firstoccurrence is not null
