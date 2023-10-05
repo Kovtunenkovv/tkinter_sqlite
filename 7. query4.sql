6603662c5c6d3c9e45f28570723cb5106aa2b373
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2023-10-05 16:01:04 +0300

    Update 7. query4.sql

--- 7. query4.sql
+++ 7. query4.sql
@@ -1,11 +1,13 @@
 SELECT hostname,zip, 
 COUNT(case when (eventid = 'POWER' or eventid = 'RECTIFIER' or eventid = 'FIRE'  or eventid = 'TEMP LOW' or eventid = 'TEMP HIGH' or eventid = 'DOOR') then 1 end) AS 'ALL',
-COUNT(case when eventid = 'POWER' then 1 end) AS 'POWER',
-COUNT(case when eventid = 'RECTIFIER' then 1 end) AS 'RECTIFIER',
-COUNT(case when eventid = 'FIRE' then 1 end) AS 'FIRE',
-COUNT(case when eventid = 'TEMP LOW' then 1 end) AS 'TEMP LOW',
-COUNT(case when eventid = 'TEMP HIGH' then 1 end) AS 'TEMP HIGH',
-COUNT(case when eventid = 'DOOR' then 1 end) AS 'DOOR'
+COUNT(case when additionalinfo1 like '%AlarmPort=1' then 1 end) AS 'POWER',
+COUNT(case when additionalinfo1 like '%AlarmPort=2' then 1 end) AS 'RECTIFIER',
+COUNT(case when additionalinfo1 like '%AlarmPort=3' then 1 end) AS 'FIRE',
+COUNT(case when additionalinfo1 like '%AlarmPort=4' then 1 end) AS 'TEMP LOW',
+COUNT(case when additionalinfo1 like '%AlarmPort=5' then 1 end) AS 'TEMP HIGH',
+COUNT(case when additionalinfo1 like '%AlarmPort=6' then 1 end) AS 'DOOR',
+COUNT(case when additionalinfo1 like '%AlarmPort=7' then 1 end) AS 'FIRE2',
+COUNT(case when additionalinfo1 like '%AlarmPort=8' then 1 end) AS 'DOOR2'
 FROM alarm_daily
 where zip = 'SEA'
 GROUP BY hostname
