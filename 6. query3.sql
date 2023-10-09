b0eb6da29cd818ea1da9d316489d0a9682e19815
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2023-10-09 16:59:14 +0300

    Update 6. query3.sql

--- 6. query3.sql
+++ 6. query3.sql
@@ -3,7 +3,8 @@ COUNT(case when zip = 'SEA' then 1 end) AS 'SEA',
 COUNT(case when zip = 'SWS' then 1 end) AS 'SWS',
 COUNT(case when zip = 'NEA' then 1 end) AS 'NEA',
 COUNT(case when zip = 'NWS' then 1 end) AS 'NWS',
-COUNT(case when zip = 'CNT' then 1 end) AS 'CNT'
+COUNT(case when zip = 'CNT' then 1 end) AS 'CNT',
+COUNT(case when zip = 'BS not use' then 1 end) AS 'BS not use'
 FROM alarm_daily
 GROUP BY eventid
 ORDER BY COUNT(eventid) DESC;
