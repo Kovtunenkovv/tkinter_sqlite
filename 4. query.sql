ec24c787575df77d133a9fb206ccab6836a7efb8
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2024-01-11 16:40:25 +0300

    Update 4. query.sql

--- 4. query.sql
+++ 4. query.sql
@@ -1,7 +1,6 @@
-CREATE TEMPORARY TABLE temp1 as select * from alarm_daily where zip = "SEA" AND eventid = "POWER" AND (firstoccurrence like "2023-09%");
+CREATE TEMPORARY TABLE temp1 as select * from alarm_daily where zip = "SEA" AND eventid = "POWER";
 
-CREATE TEMPORARY TABLE temp2 as select * from alarm_daily where zip = "SEA" AND (eventid = "Service Unavailable" or eventid = "Resource Activation Timeout") 
-AND (firstoccurrence like "2023-09%");
+CREATE TEMPORARY TABLE temp2 as select * from alarm_daily where zip = "SEA" AND (eventid = "Service Unavailable" or eventid = "Resource Activation Timeout");
 
 CREATE TEMPORARY TABLE temp3 as select hostname,e1,time1,e2,time2, ((Cast((JulianDay((time2)) - JulianDay(time1)) * 24 * 60 * 60 As Integer))) as delta, substr(substr(additionalinfo1,-4,4),2,2) as cell
 from (select hostname,e1,time1,e2,time2,additionalinfo1
