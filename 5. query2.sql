9b4d17d43a71d98217ff5e0535b8eb67284dd5ce
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2024-01-13 16:44:55 +0300

    Update 5. query2.sql

--- 5. query2.sql
+++ 5. query2.sql
@@ -9,6 +9,7 @@ where ZIP = 'SEA' and (eventid = 'Heartbeat Failure') and firstoccurrence like '
 group by firstoccurrence, hostname
 ;
 
-select p.zip, p.hostname, p.eventid ,p.firstoccurrence,p.enddate,((Cast((JulianDay((p.enddate)) - JulianDay(p.firstoccurrence)) * 24 * 60 * 60 As Integer))) as delta, h.eventid, h.firstoccurrence,h.enddate,((Cast((JulianDay((h.enddate)) - JulianDay(h.firstoccurrence)) * 24 * 60 * 60 As Integer))) as delta1,du from p
+select p.zip, p.hostname, p.eventid ,p.firstoccurrence,p.enddate,((Cast((JulianDay((p.enddate)) - JulianDay(p.firstoccurrence)) * 24 * 60 * 60 As Integer))) as delta, h.eventid, h.firstoccurrence,h.enddate,((Cast((JulianDay((h.enddate)) - JulianDay(h.firstoccurrence)) * 24 * 60 * 60 As Integer))) as delta1, du,
+((Cast((JulianDay((h.firstoccurrence)) - JulianDay(p.firstoccurrence)) * 24 * 60 * 60 As Integer))) as delta2 from p
 left join h on p.hostname = h.hostname and h.firstoccurrence BETWEEN p.firstoccurrence and p.enddate
 where h.firstoccurrence is not null
