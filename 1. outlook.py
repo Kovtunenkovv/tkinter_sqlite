ab53e017dc4dd227580c05288c13fa3f26661d6f
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2024-10-22 09:10:23 +0300

    Update and rename 1. outlook to 1. outlook.py

--- 1. outlook.py
+++ 1. outlook.py
@@ -1,27 +1,38 @@
 from exchangelib import Credentials, Account, Configuration, DELEGATE, FileAttachment, ItemAttachment, Message
 import os.path
 import datetime
+from datetime import date, timedelta
 
+data_name = datetime.datetime.today() - timedelta(days=1)
+data_name = data_name.strftime("%Y")
+nowdate = datetime.datetime.now()
 
-credentials = Credentials(username='***', password='***')
+with open('C:\\Users\\Public\\outlook.txt', 'r') as file:
+    list = file.read().splitlines()
+webmail = list[0]
+lgn = list[1]
+smtp = list[2]
+psw = list[3]
+link = list[4]
+cnt = int(list[5])
+folder = list[6]
 
-config = Configuration(server='***', credentials=credentials)
-
-account = Account(primary_smtp_address='vitaly.kovtunenko@tele2.ru', config=config, autodiscover=False, access_type=DELEGATE)
-
-path = "L:\\technical\\Эксплуатация_БС_UMTS\\Регламент_эксплуатация\\Распределение по группам\\Зона 3\\ЗИП\\!Ковтуненко\\Alarms Ericsson daily MOS 2023"   #путь до сохранения
+credentials = Credentials(username=lgn, password=psw)
+config = Configuration(server=webmail, credentials=credentials)
+account = Account(primary_smtp_address=smtp, config=config, autodiscover=False, access_type=DELEGATE)
+path = link + " " + data_name + "\\" 
 
 itemout = None
 
 nowdate = datetime.datetime.now()
 
 for item in account.inbox.children: 
-    if item.name == "Alarms Ericsson daily MOS":                                                                                        #путь в почте
+    if item.name == folder:
         itemout = item
 
 
 if itemout != None:
-    for item in itemout.all().filter(subject__contains='Alarms Ericsson daily MOS')[:3]:                                               #сколько последних вложений проверяем
+    for item in itemout.all().filter(subject__contains='Alarms Ericsson daily MOS')[:cnt]:
         for attachment in item.attachments:
             date_str = str(item.datetime_sent)
             date = datetime.datetime.strptime(date_str[:19], "%Y-%m-%d %H:%M:%S")
