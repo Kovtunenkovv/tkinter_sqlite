dcfdbbc2e214131d16a0c1ea951fc02dda8e491c
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2025-01-31 12:49:16 +0300

    Update 4. query.sql

--- 4. query.sql
+++ 4. query.sql
@@ -14,3 +14,31 @@ group by hostname
 
 select hostname,e1,maxt,e2,time2, delta from temp4 
 where delta >= 180
+
+--------------------------------------
+Combine query:
+--------------------------------------
+
+SELECT hostname, e1, maxt, e2, time2, delta
+FROM (
+    SELECT hostname, e1, time1 AS maxt, e2, time2, delta,
+           ROW_NUMBER() OVER (PARTITION BY hostname ORDER BY time1 DESC) AS rn
+    FROM (
+        SELECT hostname, e1, time1, e2, time2, 
+               ((Cast((JulianDay((time2)) - JulianDay(time1)) * 24 * 60 * 60 As Integer))) AS delta,
+               substr(substr(additionalinfo1,-4,4),2,2) AS cell
+        FROM (
+            SELECT t1.hostname, t1.firstoccurrence AS time1, t1.eventid AS e1, 
+                   t2.firstoccurrence AS time2, t2.eventid AS e2, t2.additionalinfo1
+            FROM alarm_daily t1
+            LEFT JOIN alarm_daily t2
+            ON t1.hostname = t2.hostname 
+            AND t2.firstoccurrence BETWEEN t1.firstoccurrence AND t1.enddate
+            WHERE t1.zip = "SEA" AND t1.eventid = "POWER"
+            AND t2.zip = "SEA" AND (t2.eventid = "Service Unavailable" OR t2.eventid = "Resource Activation Timeout")
+        ) AS t
+    ) AS t2
+    WHERE e2 IS NOT NULL
+    GROUP BY hostname, cell, time1
+) AS temp4
+WHERE rn = 1 AND delta >= 180;
